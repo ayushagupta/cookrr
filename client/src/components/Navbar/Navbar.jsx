a75841be0303ac3d09ba8068fccb0b70f5264ebc
ayushagupta <mumbai.ayushgupta@gmail.com>
2025-04-29 17:03:46 -0400

    add navbar and searchbar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import SearchBar from "../SearchBar/SearchBar";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleSearch = () => {};
+
+  const onClearSearch = () => {
+    setSearchQuery("");
+  };
+
+  return (
+    <div className="bg-white drop-shadow py-4 px-6 flex justify-between items-center">
+      <h1 className="text-2xl font-bold text-orange-600">Cookrr</h1>
+
+      <SearchBar
+        value={searchQuery}
+        onChange={({ target }) => {
+          setSearchQuery(target.value);
+        }}
+        handleSearch={handleSearch}
+        onClearSearch={onClearSearch}
+      />
+
+      <button className="bg-orange-600 text-white px-4 py-2 rounded-full hover:bg-orange-700 transition-colors">
+        Login
+      </button>
+    </div>
+  );
 };
 
 export default Navbar;
