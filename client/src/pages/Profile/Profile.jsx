ff92e25fa84681effdb06f38cecf4a620d348f93
ayushagupta <mumbai.ayushgupta@gmail.com>
2025-04-30 09:56:23 -0400

    fix import path bug

--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useContext } from "react";
 import { useParams } from "react-router-dom";
-import Navbar from "../components/Navbar";
+import Navbar from "../../components/Navbar/Navbar";
 
 const Profile = () => {
   const { userId } = useParams();
